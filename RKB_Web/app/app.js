9142be3501cc3f7b05c700569d5bf810adb5dd7d
Ferhat Avdic <ferhat.avdic@gmail.com>
2017-02-14 19:59:32 +0100

    landing page ready

---
+++ RKB_Web/app/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    
+    var avApp = angular.module('avApp', ['ngRoute', 'ngAnimate']);
+
+    avApp.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/pages/home/home.html',
+                controller: 'homeController'
+            })
+
+    });
+}());
---
+++ RKB_Web/app/pages/home/homeController.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    var avApp = angular.module("avApp");
+
+    avApp.controller("homeController", ['$scope', function ($scope) {
+
+        $scope.slides = [
+            { image: "/img/bik_slide.jpg", title: "Biciklizam", description: "Driving over the hills up to the mountains" },
+            { image: "/img/hik_slide.jpg", title: "Planinarenje", description: "Exploring the forests and fields of mountains" },
+            { image: "/img/ski_slide.jpg", title: "Skijanje", description: "Speeding down the lanes" },
+            { image: "/img/div_slide.jpg", title: "Ronjenje", description: "Go down into the depths of the seas" }
+        ]
+        $scope.currentSlide = 0;
+        $scope.nextSlide = function () {
+            if ($scope.currentSlide != $scope.slides.length-1) $scope.currentSlide++;
+            else $scope.currentSlide = 0;
+        }
+        $scope.previousSlide = function () {
+            if ($scope.currentSlide != 0) $scope.currentSlide--;
+            else $scope.currentSlide = $scope.slides.length - 1;
+        }
+        $scope.goToSlide = function (index) {
+            $scope.currentSlide = index;
+        }
+        $scope.dotSelected = function(index){
+            return $scope.currentSlide === index;
+        }
+
+    }]);
+}());
