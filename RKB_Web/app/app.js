419374948758e5e51646f64c41a8c7a115d6b791
Ferhat Avdic <ferhat.avdic@gmail.com>
2017-02-15 18:56:26 +0100

    home update

--- RKB_Web/app/app.js
+++ RKB_Web/app/app.js
@@ -4,10 +4,12 @@
 
     avApp.config(function ($routeProvider) {
         $routeProvider
-            .when('/', {
-                templateUrl: 'app/pages/home/home.html',
-                controller: 'homeController'
-            })
+        .when('/', { templateUrl: 'app/pages/home/home.html', controller: 'homeController' })
+        .when('/login', { templateUrl: 'app/pages/login/login.html', controller: 'loginController' })
+        .when('/blog', { templateUrl: 'app/pages/blog/blog.html', controller: 'blogController' })
+        .when('/dogadaji', { templateUrl: 'app/pages/dogadaji/dogadaji.html', controller: 'dogadajiController' })
+        .otherwise({ redirectTo: "/" });
+
 
     });
 }());
---
+++ RKB_Web/app/pages/login/loginController.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    'use strict';
+
+    var avApp = angular.module("avApp");
+
+    avApp.controller("loginController", ['$scope', function ($scope) {
+
+    }]);
+}());
