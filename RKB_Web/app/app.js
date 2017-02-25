714ea506736695ff96d1dd2448324b0db4f4b013
haris26 <haris.brackovic91@gmail.com>
2017-02-25 13:39:52 +0100

    login/signup frontend

--- RKB_Web/app/app.js
+++ RKB_Web/app/app.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     
-    var avApp = angular.module('avApp', ['ngRoute', 'ngAnimate']);
+    var avApp = angular.module('avApp', ['ngRoute', 'ngAnimate', 'LocalStorageModule']);
 
     avApp.config(function ($routeProvider) {
         $routeProvider
@@ -12,4 +12,12 @@
 
 
     });
+
+    avApp.config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authenticationInterceptorService');
+    });
+
+    avApp.run(['authenticationService', function (authenticationService) {
+        authenticationService.fillAuthData();
+    }]);
 }());
