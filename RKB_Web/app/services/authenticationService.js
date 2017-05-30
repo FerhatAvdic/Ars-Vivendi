5401321ce9f5118a4ee105d2ff4b8e22cc90f55d
Haris Brackovic <harisbr@maestralsolutions.com>
2017-05-30 13:29:24 +0200

    modifications

--- RKB_Web/app/services/authenticationService.js
+++ RKB_Web/app/services/authenticationService.js
@@ -95,8 +95,9 @@
 
             $http.get(apiSource + 'api/account/ObtainLocalAccessToken', { params: { provider: externalData.provider, externalAccessToken: externalData.externalAccessToken } }).then(function (response) {
 
-                localStorageService.set('authorizationData', { token: response.access_token, userName: response.userName, refreshToken: "", useRefreshTokens: false });
+                localStorageService.set('authorizationData', { token: response.data.access_token, userName: response.userName, refreshToken: "", useRefreshTokens: false });
 
+                console.log(response.data);
                 authentication.isAuth = true;
                 authentication.userName = response.data.userName;
                 authentication.useRefreshTokens = false;
